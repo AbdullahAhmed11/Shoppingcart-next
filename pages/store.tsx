5d2ed3c6d2c4cfe59e1b94aa7e6e336223ac36f4
Abdullahahmed11 <abdullah77zzx@gmail.com>
2023-08-13 13:57:09 +0300

    createStore

---
+++ pages/store.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Store() {
+    return (
+        <>
+            <div>
+                <h1>Hello, Store</h1>
+            </div>
+        </>
+    )
+}
+export default Store;
